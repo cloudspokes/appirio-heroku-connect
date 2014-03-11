9df167a29c42d45de3e7cb23ac38ed61e95681b1
Jeff Douglas <jeff@jeffdouglas.com>
2014-03-11 11:08:50 -0400

    added in actual app

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
-var http = require("http");
-var connect = require('connect');
+var express = require('express')
+  , routes = require('./routes')
 
-var app = connect()
-    .use(connect.static('home'))
-    .use(function(req, res){
- });
+var port = process.env.PORT || 5000; // use heroku's dynamic port or 5000 if localhost
 
-var server = http.createServer(app);
+// create the server
+var app = module.exports = express.createServer();
 
-server.listen(9999, function () {
-    console.log('server is listening');
+// Configuration
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+// Routes
+app.get('/', routes.index);
+
+app.listen(port, function(){
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
